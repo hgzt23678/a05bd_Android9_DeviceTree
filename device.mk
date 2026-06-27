422e063cb509528fa4d0ff7fdfacc6d9ff2e7ed2
hgzt23678 <kakunin0080@gmail.com>
2026-06-27 16:06:21 +0900

    Align A05BD device tree with stock config

--- device.mk
+++ device.mk
@@ -2,14 +2,10 @@ PRODUCT_SHIPPING_API_LEVEL := 28
 
 
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service \
     android.hardware.bluetooth@1.0-service-mediatek \
     android.hardware.light@2.0-service-mediatek \
     android.hardware.audio@4.0-service-mediatek \
     android.hardware.audio.effect@4.0-service-mediatek \
-    android.hardware.wifi@1.0-service \
     android.hardware.wifi@1.0-service-mediatek \
     wpa_supplicant \
     wificond \
@@ -22,8 +18,7 @@ PRODUCT_PACKAGES += \
     libdpm \
     libnl \
     libpcap \
-    wifi-service \
-    WifiOverlay
+    wifi-service
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
@@ -44,22 +39,6 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     qemu.hw.mainkeys=0
 
-PRODUCT_PACKAGES += \
-    fstab.mt8168 \
-    init.ago.rc \
-    init.connectivity.rc \
-    init.modem.rc \
-    init.mt8168.rc \
-    init.mt8168.usb.rc \
-    init.project.rc \
-    init.sensor_1_0.rc \
-    meta_init.connectivity.rc \
-    meta_init.modem.rc \
-    meta_init.project.rc \
-    meta_init.rc \
-    multi_init.rc \
-
-
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
@@ -68,18 +47,18 @@ $(call inherit-product, vendor/sts/a05bd/a05bd-vendor.mk)
 
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/etc/init.ago.rc:vendor/etc/init/init.ago.rc \
-    $(LOCAL_PATH)/etc/init.connectivity.rc:vendor/etc/init/init.connectivity.rc \
-    $(LOCAL_PATH)/etc/init.modem.rc:vendor/etc/init/init.modem.rc \
-    $(LOCAL_PATH)/etc/init.mt8168.rc:vendor/etc/init/init.mt8168.rc \
-    $(LOCAL_PATH)/etc/init.mt8168.usb.rc:vendor/etc/init/init.mt8168.usb.rc \
-    $(LOCAL_PATH)/etc/init.project.rc:vendor/etc/init/init.project.rc \
-    $(LOCAL_PATH)/etc/init.sensor_1_0.rc:vendor/etc/init/init.sensor_1_0.rc \
-    $(LOCAL_PATH)/etc/meta_init.connectivity.rc:vendor/etc/init/meta_init.connectivity.rc \
-    $(LOCAL_PATH)/etc/meta_init.modem.rc:vendor/etc/init/meta_init.modem.rc \
-    $(LOCAL_PATH)/etc/meta_init.project.rc:vendor/etc/init/meta_init.project.rc \
-    $(LOCAL_PATH)/etc/meta_init.rc:vendor/etc/init/meta_init.rc \
-    $(LOCAL_PATH)/etc/multi_init.rc:vendor/etc/init/multi_init.rc \
+    $(LOCAL_PATH)/etc/init.ago.rc:vendor/etc/init/hw/init.ago.rc \
+    $(LOCAL_PATH)/etc/init.connectivity.rc:vendor/etc/init/hw/init.connectivity.rc \
+    $(LOCAL_PATH)/etc/init.modem.rc:vendor/etc/init/hw/init.modem.rc \
+    $(LOCAL_PATH)/etc/init.mt8168.rc:vendor/etc/init/hw/init.mt8168.rc \
+    $(LOCAL_PATH)/etc/init.mt8168.usb.rc:vendor/etc/init/hw/init.mt8168.usb.rc \
+    $(LOCAL_PATH)/etc/init.project.rc:vendor/etc/init/hw/init.project.rc \
+    $(LOCAL_PATH)/etc/init.sensor_1_0.rc:vendor/etc/init/hw/init.sensor_1_0.rc \
+    $(LOCAL_PATH)/etc/meta_init.connectivity.rc:vendor/etc/init/hw/meta_init.connectivity.rc \
+    $(LOCAL_PATH)/etc/meta_init.modem.rc:vendor/etc/init/hw/meta_init.modem.rc \
+    $(LOCAL_PATH)/etc/meta_init.project.rc:vendor/etc/init/hw/meta_init.project.rc \
+    $(LOCAL_PATH)/etc/meta_init.rc:vendor/etc/init/hw/meta_init.rc \
+    $(LOCAL_PATH)/etc/multi_init.rc:vendor/etc/init/hw/multi_init.rc \
     $(LOCAL_PATH)/etc/fstab.mt8168:vendor/etc/fstab.mt8168 \
     $(LOCAL_PATH)/etc/ueventd.rc:vendor/ueventd.rc \
     $(LOCAL_PATH)/TouchPanel/Wacom_I2C_AES.idc:system/usr/idc/Wacom_I2C_AES.idc \
